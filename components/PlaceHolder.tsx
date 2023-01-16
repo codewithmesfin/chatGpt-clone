7f47506ef4327db945c53e0af26250055f8e8f61
System Administrator <sciemesfin55@gmail.com>
2023-01-16 22:15:09 +0300

    save

--- components/PlaceHolder.tsx
+++ components/PlaceHolder.tsx
@@ -96,7 +96,7 @@ export default function PlaceHolder({ onPlaceholderClick }: PROPS) {
           <div className="flex flex-col items-center text-sm h-full dark:bg-gray-800">
             <div className="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100">
               <h1 className="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16">
-               EtyopChat
+               BizeChat
               </h1>
               <div className="md:flex items-start text-center gap-3.5">
                 {items.map((item: any, i: number) => (
